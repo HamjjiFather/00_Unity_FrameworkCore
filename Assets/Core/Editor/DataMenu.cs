3b16d1be330b768f61ef76466e64062f28298c09
금광섭 <rhkdtjq0390@gmail.com>
2021-05-16 00:07:40 +0900

    -

--- Assets/Core/Editor/DataMenu.cs
+++ Assets/Core/Editor/DataMenu.cs
@@ -7,27 +7,27 @@ namespace KKSFramework.Editor
 {
     public static class DataFolderEditor
     {
-        [MenuItem ("Framework/Folder/OpenCacheFolder", priority = 3)]
+        [MenuItem ("Framework/Folder/Open Temporary Cache Folder", priority = 3)]
         public static void ShowTemporaryCacheFolder ()
         {
             Process.Start (Application.temporaryCachePath);
         }
 
-        [MenuItem ("Framework/Folder/OpenPersistentFolder", priority = 2)]
+        [MenuItem ("Framework/Folder/Open Persistent Data Folder", priority = 2)]
         public static void ShowPersistentFolder ()
         {
             Process.Start (Application.persistentDataPath);
         }
 
 
-        [MenuItem ("Framework/Folder/OpenDataPath", priority = 1)]
+        [MenuItem ("Framework/Folder/Open Data Path", priority = 1)]
         public static void ShowMainFolder ()
         {
             Process.Start (Application.dataPath);
         }
         
         
-        [MenuItem ("Framework/Data/Delete Local Data")]
+        [MenuItem ("Framework/Data/Delete Persistent Data")]
         public static void DeleteLocalData ()
         {
             PlayerPrefs.DeleteAll ();
